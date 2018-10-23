320c570de7b560627f6e5683d3bc32ec8fa7e76b
ryongpintu <ryongpintu@gmail.com>
2018-10-24 02:07:29 +0530

    added google oauth

--- app/app.js
+++ app/app.js
@@ -1,11 +1,15 @@
 const  mogoose = require('mongoose');
 const express= require('express');
-
+const auth = require('./routes/auth');
+const passport = require('passport');
 const app= express();
 
+require('./config/passport')(passport)
 app.get('/',(req,res)=>{
   res.send('Its work');
-})
+});
+
+app.use('/auth',auth);
 const port =process.env.PORT || 8000;
 app.listen(port,(res)=>{
   console.log(`listening to port ${port}`)
---
+++ app/config/keys.js
@@ -0,0 +1,5 @@
+module.exports={
+  mongoURI:'mongodb://ryong:pintu123@ds239873.mlab.com:39873/storybook',
+  googleClientID:'80389814395-373e8jaj79goro01plnk17i1l8mvdmjn.apps.googleusercontent.com',
+  googleClientSecret:'AZA21eeukE_JFt1otnpkBp1x'
+}
