f972716d8064477bef966b417213593f7e91470b
ryongpintu <ryongpintu@gmail.com>
2018-10-24 02:45:29 +0530

    added view

--- app/app.js
+++ app/app.js
@@ -3,10 +3,16 @@ const express= require('express');
 const auth = require('./routes/auth');
 const passport = require('passport');
 const app= express();
-
+const exphbs=require('express-handlebars');
 require('./config/passport')(passport)
+
+app.engine('handlebars',exphbs({
+  defaultLayout:'main'
+}));
+app.set('view engine','handlebars');
+
 app.get('/',(req,res)=>{
-  res.send('Its work');
+  res.render('index');
 });
 
 app.use('/auth',auth);
